263f928a8c44d8a568b8b904176069df3c5a14a0
Isidora Djurdjevic <isidoradjurdjevic.100@gmail.com>
2018-05-16 23:49:40 +0200

    Added settings.py for global variables

---
+++ settings.py
@@ -0,0 +1,10 @@
+import numpy as np
+
+def init():
+    SCORE_MATRIX = np.zeros((10,10))
+    score_matrix = np.zeros((10,10))
+    GIVEN = False
+    GIVEN2 = False
+    CUSTOM = False
+    G = False
+    L = False
